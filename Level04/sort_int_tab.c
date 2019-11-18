59d6d09e6286686dc05a80985cef3221bfe28770
HalimHamidov <halimjonik@gmail.com>
2019-11-18 21:57:48 +0300

    l4-sort_in_tab f_prime gdb

--- Level04/sort_int_tab.c
+++ Level04/sort_int_tab.c
@@ -1,68 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-unsigned int    ft_strlen(int *str)
+void    sort_in_tab(int *tab, unsigned int size)
 {
-    int i;
+    unsigned int i;
+    int tmp;
     
     i = 0;
-    while (str[i])
-        i++;
-    return (i);
-}
-void sort_int_tab(int *tab, unsigned int size)
-{
-	unsigned int i = 0;
-	unsigned int j;
-	int temp;
-
-	while ( i < size)
-	{
-		j = i + 1;
-		while (j < size)
-		{
-			if (tab[i] > tab[j])
-			{
-				temp = tab[i];
-				tab[i] = tab[j];
-				tab[j] = temp;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-void    print(int *tab)
-{
-    int i;
-    i = 0;
-    while (tab[i])
+    if (size == 0)
+        return ;
+    while (i < size - 1)
     {
-        printf("[%d] - ", tab[i]);
-        i++;
+        if (tab[i] > tab[i + 1])
+        {
+            tmp = tab[i + 1];
+            tab[i + 1] = tab[i];
+            tab[i] = tmp;
+            i = 0;
+        }
+        else
+            i++;
     }
 }
-int        main(void)
-{
-    int *tab;
-    
-    if (!(tab = (int*)malloc(sizeof(int) * 10 + 1)))
-        return (0);
-    tab[0] = 7;
-    tab[1] = 5;
-    tab[2] = 4;
-    tab[3] = 2;
-    tab[4] = 4;
-    tab[5] = 4;
-    tab[6] = 4;
-    tab[7] = 4;
-    tab[8] = 3;
-    tab[9] = 2;
-    tab[10] = '\0';
-    sort_int_tab(tab, ft_strlen(tab));
-    print(tab);
-    return (0);
-}
+
 //
 //Assignment name  : sort_int_tab
 //Expected files   : sort_int_tab.c
